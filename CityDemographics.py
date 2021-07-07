4bc0ef4538e9ea5fedb36be8a1586cfe74ae70eb
Redbeard29 <ben@benshermanmusic.com>
2021-07-07 19:53:04 -0400

    initialized receiving dataframe

--- CityDemographics.py
+++ CityDemographics.py
@@ -14,18 +14,16 @@ GBR_DATA = pd.read_csv(path.join(BASE_DIR, 'GreensboroCityRaceSexAge.csv'))
 PIT_DATA = pd.read_csv(path.join(BASE_DIR, 'PittsburghCityRaceSexAge.csv'))
 RAL_DATA = pd.read_csv(path.join(BASE_DIR, 'RaleighCityRaceSexAge.csv'))
 
+city_comparison_df = pd.DataFrame(columns=['TotalPop'], 
+    index=['Atlanta', 'Austin', 'Boston', 'Charlotte', 'Dallas', 'D.C.', 'Greensboro', 'Pittsburgh', 'Raleigh'])
+
 def get_total_pop(csv_file, city_name, state_name=None):
     if(state_name == None):
         return csv_file[city_name + '!!2019 Estimate'][34]
     else:
         return csv_file[city_name +' city, ' + state_name + '!!2019 Estimate'][34]
 
-#redo this with inheritance to save code:
-def get_demo_percentages(csv_file, city_name, state_name=None):
-    if(state_name == None):
-        return csv_file[city_name + '!!2019 Estimate'][38]
-    else:
-        return csv_file[city_name +' city, ' + state_name + '!!2019 Estimate'][38]
+#define tuples to save rewriting code?:
 
 ATL_population = get_total_pop(ATL_DATA, 'Atlanta', 'Georgia')
 ATX_population = get_total_pop(ATX_DATA, 'Austin', 'Texas')
@@ -36,3 +34,7 @@ DC_population = get_total_pop(DC_DATA, 'District of Columbia')
 GBR_population = get_total_pop(GBR_DATA, 'Greensboro', 'North Carolina')
 PIT_population = get_total_pop(PIT_DATA, 'Pittsburgh', 'Pennsylvania')
 RAL_population = get_total_pop(RAL_DATA, 'Raleigh', 'North Carolina')
+
+city_comparison_df['TotalPop'] = [ATL_population, ATX_population, BOS_population, CHA_population, DTX_population, DC_population, GBR_population, PIT_population, RAL_population]
+
+print(city_comparison_df)
