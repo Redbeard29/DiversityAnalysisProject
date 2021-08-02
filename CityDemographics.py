6e1112264dcb1bb726defe90ac8564eb4b1d3438
Redbeard29 <ben@benshermanmusic.com>
2021-08-02 11:04:15 -0400

    added mean income per household, per capita, and per capita by race

--- CityDemographics.py
+++ CityDemographics.py
@@ -52,14 +52,14 @@ SEA_MEAN_INCOME = path.join(BASE_DIR, 'Income/SeattleCityMeanIncome.csv')
 SEA_MEDIAN_INCOME = path.join(BASE_DIR, 'Income/SeattleCityMedianIncome.csv')
 
 #Cost of living index:
-COL_INDEX = path.join(BASE_DIR, 'Income/CostOfLivingIndex.csv')
+# COL_INDEX = path.join(BASE_DIR, 'Income/CostOfLivingIndex.csv')
 
 
 #Define the DataFrame that we're writing our new data to
-city_comparison_df = pd.DataFrame(columns=['TotalPop', 'White','Black', 'Asian', 'NativeAm', 'Other', 'TwoOrMoreRaces', 'BlackAndWhite', 'TotalPopMeanIncome'],
+city_comparison_df = pd.DataFrame(columns=['TotalPop', 'White','Black', 'Asian', 'NativeAm', 'Other', 'TwoOrMoreRaces', 'BlackAndWhite', 'MeanIncomeHousehold', 'MeanIncomePerCap', 'MIPCWhite', 'MIPCBlack', 'MIPCWhitePercOfTot', 'MIPCBlackPercOfTot'],
     index=['Atlanta', 'Austin', 'Baltimore', 'Boston', 'Charlotte', 'Dallas', 'District of Columbia', 'Greensboro', 'Los Angeles', 'New York', 'Pittsburgh', 'Raleigh', 'San Francisco', 'Seattle'])
 
-#Define dicts with file path, city and state for each location so that we can pass them in to all of our necessary functions. 
+#Define dicts with file paths for each csv, city and state for each location so that we can pass them in to all of our necessary functions. 
 #This formatting is required to read the census csv files:
 
 ATL_dict = {'demo_csv' : ATL_DEMO, 'mean_csv' : ATL_MEAN_INCOME, 'median_csv' : ATL_MEDIAN_INCOME, 'city_name': 'Atlanta', 'state_name': 'Georgia'}
@@ -78,12 +78,14 @@ SF_dict = {'demo_csv' : SF_DEMO, 'mean_csv' : SF_MEAN_INCOME, 'median_csv' : SF_
 SEA_dict = {'demo_csv' : SEA_DEMO, 'mean_csv' : SEA_MEAN_INCOME, 'median_csv' : SEA_MEDIAN_INCOME, 'city_name': 'Seattle', 'state_name': 'Washington'}
 
 dict_list = [ATL_dict, ATX_dict, BAL_dict, BOS_dict, CHA_dict, DTX_dict, DC_dict, GBR_dict, LA_dict, NYC_dict, PIT_dict, RAL_dict, SF_dict, SEA_dict]
-nums_list = [34, 38, 39, 45, 40, 58, 59, 60]
-cols_list = ['TotalPop', 'White','Black', 'Asian', 'NativeAm', 'Other', 'TwoOrMoreRaces', 'BlackAndWhite']
+
 
 #Add demographic data to csv:
 
-def get_demographic_data(dict_list, nums_list, cols_list):
+def get_demographic_data(dict_list):
+
+    nums_list = [34, 38, 39, 45, 40, 58, 59, 60]
+    cols_list = ['TotalPop', 'White','Black', 'Asian', 'NativeAm', 'Other', 'TwoOrMoreRaces', 'BlackAndWhite']
 
     for x in range(len(dict_list)):
         city_dict = dict_list.pop()
@@ -95,58 +97,76 @@ def get_demographic_data(dict_list, nums_list, cols_list):
         col_IDX = 0
 
         while(num_IDX < len(nums_list) and col_IDX < len(cols_list)):
+
             if(state_name == None):
                 city_comparison_df[cols_list[col_IDX]][city_name] = csv_file[city_name + '!!2019 Estimate'][nums_list[num_IDX]]
-                col_IDX += 1
-                num_IDX += 1
             else:
                 city_comparison_df[cols_list[col_IDX]][city_name] = csv_file[city_name +' city, ' + state_name + '!!2019 Estimate'][nums_list[num_IDX]]
-                col_IDX += 1
-                num_IDX += 1
+           
+            col_IDX += 1
+            num_IDX += 1
+    
+        get_economic_data(city_dict)
 
-get_demographic_data(dict_list, nums_list, cols_list)
 
+def get_economic_data(city_dict):
 
-read_atl_income = pd.read_csv(ATL_MEAN_INCOME)
+    nums_list = [1, 21, 23, 24, 0, 0]
+    cols_list = ['MeanIncomeHousehold', 'MeanIncomePerCap', 'MIPCWhite', 'MIPCBlack', 'MIPCWhitePercOfTot', 'MIPCBlackPercOfTot']
 
-print(read_atl_income['Atlanta city, Georgia!!Mean income (dollars)!!Estimate'][21])
-
-def get_economic_data(dict_list):
-
-    for x in range(len(dict_list)):
-        city_dict = dict_list.pop()
+    for x in range(len(city_dict)):
         csv_file = pd.read_csv(city_dict['mean_csv'])
         city_name = city_dict['city_name']
         state_name = city_dict['state_name']
 
-        print(city_dict)
+        num_IDX = 0
+        col_IDX = 0
 
-        if(state_name == None):
-            city_comparison_df['TotalPopMeanIncome'][city_name] = csv_file[city_name + '!!Mean income (dollars)!!Estimate'][21]
 
-        else:
-            city_comparison_df['TotalPopMeanIncome'][city_name] = csv_file[city_name +' city, ' + state_name + '!!Mean income (dollars)!!Estimate'][21]
+        while(num_IDX < len(nums_list) and col_IDX < len(cols_list)):
 
-get_economic_data(dict_list)
+            current_col = [cols_list[col_IDX]]
+
+            if current_col == (['MIPCWhitePercOfTot'] or ['MIPCBlackPercOfTot']):
+                calculate_percentage(city_dict)
+
+            if(state_name == None):
+                city_comparison_df[cols_list[col_IDX]][city_name] = csv_file[city_name + '!!Mean income (dollars)!!Estimate'][nums_list[num_IDX]]
+
+            else:
+                city_comparison_df[cols_list[col_IDX]][city_name] = csv_file[city_name +' city, ' + state_name + '!!Mean income (dollars)!!Estimate'][nums_list[num_IDX]]
+
+            col_IDX += 1
+            num_IDX += 1
+
+def calculate_percentage(city_dict):
+    csv_file = pd.read_csv(city_dict['mean_csv'])
+    city_name = city_dict['city_name']
+    state_name = city_dict['state_name']
+    print(city_dict)
+
+
+get_demographic_data(dict_list)
+
 
 #def col_calculator(col_dict):
     #total_col = col_dict['Total']
 
 #Define cost of living dictionaries to pass to col_calculator function:
-ATL_COL = {'Total' : 76.76, 'Rent' : 50.85, 'COLI Plus Rent' : 64.77, 'Groceries' : 78.2, 'Restaurant Price' : 68.66}
-ATX_COL = {'Total' : 63.84, 'Rent' : 54.47, 'COLI Plus Rent' : 59.51, 'Groceries' : 63.59, 'Restaurant Price' : 68.06}
-BAL_COL = {'Total' : 72.47, 'Rent' : 44.69, 'COLI Plus Rent' : 59.62, 'Groceries' : 70.62, 'Restaurant Price' : 73.92}
-BOS_COL = {'Total' : 88.67, 'Rent' : 77.37, 'COLI Plus Rent' : 83.44, 'Groceries' : 88.36, 'Restaurant Price' : 94.53}
-CHA_COL = {'Total' : 69.62, 'Rent' : 48.86, 'COLI Plus Rent' : 60.03, 'Groceries' : 66.76, 'Restaurant Price' : 68.8}
-DTX_COL = {'Total' : 67.68, 'Rent' : 50.13, 'COLI Plus Rent' : 59.57, 'Groceries' : 62.25, 'Restaurant Price' : 72.52}
-DC_COL = {'Total' : 83.91, 'Rent' : 78.24, 'COLI Plus Rent' : 81.29, 'Groceries' : 83.89, 'Restaurant Price' : 79.4}
-GBR_COL = {'Total' : None, 'Rent' : None, 'COLI Plus Rent' : None, 'Groceries' : None, 'Restaurant Price' : None}
-LA_COL = {'Total' : 79.6, 'Rent' : 74.08, 'COLI Plus Rent' : 77.05, 'Groceries' : 75.95, 'Restaurant Price' : 91.55}
-NYC_COL = {'Total' : 100, 'Rent' : 100, 'COLI Plus Rent' : 100, 'Groceries' : 100, 'Restaurant Price' : 100}
-PIT_COL = {'Total' : 78.46, 'Rent' : 39.82, 'COLI Plus Rent' : 60.6, 'Groceries' : 84.4, 'Restaurant Price' : 61.6}
-RAL_COL = {'Total' : 68.76, 'Rent' : 39.22, 'COLI Plus Rent' : 55.1, 'Groceries' : 70.8, 'Restaurant Price' : 70.47}
-SF_COL = {'Total' : 93.89, 'Rent' : 118.15, 'COLI Plus Rent' : 105.1, 'Groceries' : 96.89, 'Restaurant Price' : 94.37}
-SEA_COL = {'Total' : 86.38, 'Rent' : 71.02, 'COLI Plus Rent' : 79.28, 'Groceries' : 84.54, 'Restaurant Price' : 86.21}
+# ATL_COL = {'Total' : 76.76, 'Rent' : 50.85, 'COLI Plus Rent' : 64.77, 'Groceries' : 78.2, 'Restaurant Price' : 68.66}
+# ATX_COL = {'Total' : 63.84, 'Rent' : 54.47, 'COLI Plus Rent' : 59.51, 'Groceries' : 63.59, 'Restaurant Price' : 68.06}
+# BAL_COL = {'Total' : 72.47, 'Rent' : 44.69, 'COLI Plus Rent' : 59.62, 'Groceries' : 70.62, 'Restaurant Price' : 73.92}
+# BOS_COL = {'Total' : 88.67, 'Rent' : 77.37, 'COLI Plus Rent' : 83.44, 'Groceries' : 88.36, 'Restaurant Price' : 94.53}
+# CHA_COL = {'Total' : 69.62, 'Rent' : 48.86, 'COLI Plus Rent' : 60.03, 'Groceries' : 66.76, 'Restaurant Price' : 68.8}
+# DTX_COL = {'Total' : 67.68, 'Rent' : 50.13, 'COLI Plus Rent' : 59.57, 'Groceries' : 62.25, 'Restaurant Price' : 72.52}
+# DC_COL = {'Total' : 83.91, 'Rent' : 78.24, 'COLI Plus Rent' : 81.29, 'Groceries' : 83.89, 'Restaurant Price' : 79.4}
+# GBR_COL = {'Total' : None, 'Rent' : None, 'COLI Plus Rent' : None, 'Groceries' : None, 'Restaurant Price' : None}
+# LA_COL = {'Total' : 79.6, 'Rent' : 74.08, 'COLI Plus Rent' : 77.05, 'Groceries' : 75.95, 'Restaurant Price' : 91.55}
+# NYC_COL = {'Total' : 100, 'Rent' : 100, 'COLI Plus Rent' : 100, 'Groceries' : 100, 'Restaurant Price' : 100}
+# PIT_COL = {'Total' : 78.46, 'Rent' : 39.82, 'COLI Plus Rent' : 60.6, 'Groceries' : 84.4, 'Restaurant Price' : 61.6}
+# RAL_COL = {'Total' : 68.76, 'Rent' : 39.22, 'COLI Plus Rent' : 55.1, 'Groceries' : 70.8, 'Restaurant Price' : 70.47}
+# SF_COL = {'Total' : 93.89, 'Rent' : 118.15, 'COLI Plus Rent' : 105.1, 'Groceries' : 96.89, 'Restaurant Price' : 94.37}
+# SEA_COL = {'Total' : 86.38, 'Rent' : 71.02, 'COLI Plus Rent' : 79.28, 'Groceries' : 84.54, 'Restaurant Price' : 86.21}
 
 # one_percent_rent = 42.189854344550476
 
@@ -157,3 +177,5 @@ SEA_COL = {'Total' : 86.38, 'Rent' : 71.02, 'COLI Plus Rent' : 79.28, 'Groceries
 city_comparison_df.to_csv(path.join(BASE_DIR, 'Analysis/CityComparison.csv'))
 
 print(city_comparison_df)
+
+# print(90005/54414)
